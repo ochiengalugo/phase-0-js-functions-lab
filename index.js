a93ba7cde48df25d5fd6f655c575384304063d9f
ochiengalugo <ochieng.alugo@student.moringaschool.com>
2026-03-21 10:39:04 +0300

    added js

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
 
+//calculateTax
+function calculateTax(amount) {
+    return amount * 0.10;
+}
+const taxResult = calculateTax(10000);
+console.log(taxResult);
+
+//convert to uppercase
+function convertToUpperCase(text) {
+    return text.toUpperCase();}
+    
+
+const results = convertToUpperCase("hello world")
+console.log(results)
 
 
+// Function 3: findMaximum
+function findMaximum(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+const largest = findMaximum(10, 5);
+console.log("The larger number is: " + largest); 
 
 
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+// Function 4: isPalindrome
+function isPalindrome(word) {
+    let reversedWord = word.split("").reverse().join("");
+    return word === reversedWord;
+}
+const checkWord = isPalindrome("radar");
+console.log(checkWord);
+
+
+// Function 5: calculateDiscountedPrice
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    let discountAmount = originalPrice * (discountPercentage / 100);
+    
+    return originalPrice - discountAmount;
+}
+const finalPrice = calculateDiscountedPrice(100, 20);
+console.log(finalPrice);
+
